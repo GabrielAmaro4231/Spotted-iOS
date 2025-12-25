5fc204c0eb21f9b38188cc9ab2ebbcbfd89f4674
Gabriel Amaro <gabrielamaro4231@gmail.com>
2025-12-25 16:38:00 -0300

    Initial Commit

---
+++ Spotted/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Spotted
+//
+//  Created by Gabriel Amaro on 25/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Spotted/SpottedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpottedApp.swift
+//  Spotted
+//
+//  Created by Gabriel Amaro on 25/12/25.
+//
+
+import SwiftUI
+
+@main
+struct SpottedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
